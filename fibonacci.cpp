bfa758788fbd9acc61a8b06bc4845b295977c6cd
Josh Orndorff <orndorff.josh@gmail.com>
2012-12-01 17:55:26 -0500

    The code actually works now, and has a check to avoid overflow

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <ncurses.h>
 using namespace std;
+
+
+int nextNumber (int prev1, int prev2);
 
 int main ()
 {
-  cout << "Generating the Fibonacci Series starting with 1 and 1.\n";
+  int start1 = 1;
+  int start2 = 2;
+
+  cout << "Generating the Fibonacci Series starting with" << start1 << " and " << start2 << ".\n";
+  cout << start1 << endl;
+  cout << start2 << endl;
+
+  nextNumber(start1, start2);
+
+  return 0;
+}
+
+int nextNumber (int prev1, int prev2)
+{
+  int next = prev1 + prev2;
+
+  if(next < 0){
+    cout << "Really?  You need this many Fibonacci's?  We just overflowed the int type!\n\n\n";
+  } else {
+    cout << next << "   Press enter to generate another... ";
+    cin.get();
+    nextNumber (prev2, next);
+  }
   return 0;
 }
